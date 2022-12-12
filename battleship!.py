868361f74965ad662054d6d5d801c9c0e8b9d9ab
ehochberg23 <ehochberg23@gcds.net>
2022-12-12 11:55:30 -0500

    yeah

--- battleship!.py
+++ battleship!.py
@@ -1,6 +1,6 @@
 """
 Name: Ellie Hochberg
-Date: Nov 30, 2022
+Date: Dec 16, 2022
 Description: Play a game of battleship with the computer
 Bugs: No bugs
 Plan:
@@ -8,7 +8,7 @@ Plan:
     2)Ask the player where they want to shoot
     3)Put a * if they didn't hit a ship and a ! if they did on the computer's board
     4)Computer shoots a random spot
-    5)Put a * if they didn't hit a ship and a ! if they did on the players's board
+    5)Put a * if they didn't hit a ship and a ! if they did on the player's board
     6)Repeat until there is a winner
 """
 import random
@@ -70,41 +70,46 @@ def changeBoard(board, game):
             print()
 
 
-def playerChoice(board2):
+def playerChoice(compBoard):
     """
     The player's choice of where to shoot
     Arguments:
-        board2: the computer's board
+        compBoard: the computer's board
     Returns:
         A * on the board if there is no ship in that spot
         A ! on the board if there is a ship in that spot
     """
 
-    r = int(input("Pick a row"))            #row
-    c = int(input("Pick a column"))           #column
+    r = int(input("Pick a row"))           #row
+    c = int(input("Pick a column"))       #column
 
-    if 0>c>4 or 0>r>4:                        #if number smaller than 0 or bigger than 2
-        print("Please enter valid numbers")
-        playerChoice(board2, board2)
+    if type(r) or type(c) is str:
+        print("Please enter numbers")
+        playerChoice(compBoard)
 
-    elif (board2[r][c] == "X"):                         #if ship in that spot
-        board2[r][c] = "!"
+
+    if 0>r>4 or 0>c>4:
+        print("Choose a valid number")
+        playerChoice(compBoard)
+
+    elif (compBoard[r][c] == "X"):                         #if ship in that spot
+        compBoard[r][c] = "!"
         print("You hit a ship!")
 
-    elif (board2[r][c] == "*") or (board2[r][c] == "!"):                  #if spot already taken
+    elif (compBoard[r][c] == "*") or (compBoard[r][c] == "!"):                  #if spot already taken
         print("Spot already chosen. Choose again")
-        playerChoice(board2)
+        playerChoice(compBoard)
 
     else:
-        board2[r][c] = "*"                        #put an X where the player chose
+        compBoard[r][c] = "*"                        #put a * where the player chose
 
-    return board2
+    return compBoard
 
-def computerChoice(board1):
+def computerChoice(playerBoard):
     """
     Randomize the computer's choice of where to shoot
     Arguments:
-        board1: the player's board
+        playerBoard: the player's board
     Returns:
         A * on the board if there is no ship in that spot
         A ! on the board if there is a ship in that spot
@@ -113,32 +118,33 @@ def computerChoice(board1):
     r = random.randint(0,4)                         #random r from 0-2
     c = random.randint(0,4)                          #random c from 0-2
 
-    if (board1[r][c] == "X"):                     #if ship in that spot
-        board1[r][c] = "!"
+    if (playerBoard[r][c] == "X"):                     #if ship in that spot
+        playerBoard[r][c] = "!"
         print("Computer hit your ship")
 
-    elif (board1[r][c] == "*") or (board1[r][c] == "!"):                  #if spot already taken
-        playerChoice(board1)
+    elif (playerBoard[r][c] == "*") or (playerBoard[r][c] == "!"):                  #if spot already taken
+        playerChoice(playerBoard)
     else:
-        board1[r][c] = "*"                                 #put an * in the spot chosen
+        playerBoard[r][c] = "*"                                 #put an * in the spot chosen
 
-    return board1
+    return playerBoard
 
-def winGame(board1, board2):
+
+def winGame(playerBoard, compBoard):
     """
     Conditions to win the game
     Arguments:
-        board1: the player's board
-        board2: the computer's board
+        playerBoard: the player's board
+        compBoard: the computer's board
     Returns:
         Determine if there's a winner
     """
 
-    if (board1[0][0] != "X") and (board1[0][1] != "X") and (board1[0][2] != "X")and (board1[0][3] != "X") and (board1[0][4] != "X") and (board1[1][0] != "X") and (board1[1][1] != "X") and (board1[1][2] != "X") and (board1[1][3] != "X") and (board1[1][4] != "X") and (board1[2][0] != "X") and (board1[2][1] != "X") and (board1[2][2] != "X") and (board1[2][3] != "X") and (board1[2][4] != "X") and (board1[3][0] != "X") and (board1[3][1] != "X")and (board1[3][2] != "X") and (board1[3][3] != "X") and (board1[3][4] != "X") and (board1[4][0] != "X") and (board1[4][1] != "X") and (board1[4][2] != "X")and (board1[4][3] != "X") and (board1[4][4] != "X"):
+    if (playerBoard[0][0] != "X") and (playerBoard[0][1] != "X") and (playerBoard[0][2] != "X")and (playerBoard[0][3] != "X") and (playerBoard[0][4] != "X") and (playerBoard[1][0] != "X") and (playerBoard[1][1] != "X") and (playerBoard[1][2] != "X") and (playerBoard[1][3] != "X") and (playerBoard[1][4] != "X") and (playerBoard[2][0] != "X") and (playerBoard[2][1] != "X") and (playerBoard[2][2] != "X") and (playerBoard[2][3] != "X") and (playerBoard[2][4] != "X") and (playerBoard[3][0] != "X") and (playerBoard[3][1] != "X")and (playerBoard[3][2] != "X") and (playerBoard[3][3] != "X") and (playerBoard[3][4] != "X") and (playerBoard[4][0] != "X") and (playerBoard[4][1] != "X") and (playerBoard[4][2] != "X")and (playerBoard[4][3] != "X") and (playerBoard[4][4] != "X"):
         print("Computer wins")       #if all the X's are hit on the player's board
         return 1
 
-    elif (board2[0][0] != "X") and (board2[0][1] != "X") and (board2[0][2] != "X") and (board2[0][3] != "X") and (board2[0][4] != "X") and (board2[1][0] != "X") and (board2[1][1] != "X") and (board2[1][2] != "X") and (board2[1][3] != "X") and (board2[1][4] != "X") and (board2[2][0] != "X") and (board2[2][1] != "X") and (board2[2][2] != "X") and (board2[2][3] != "X") and (board2[2][4] != "X") and (board2[3][0] != "X") and (board2[3][1] != "X")and (board2[3][2] != "X") and (board2[3][3] != "X") and (board2[3][4] != "X") and (board2[4][0] != "X") and (board2[4][1] != "X") and (board2[4][2] != "X")and (board2[4][3] != "X") and (board2[4][4] != "X"):
+    elif (compBoard[0][0] != "X") and (compBoard[0][1] != "X") and (compBoard[0][2] != "X") and (compBoard[0][3] != "X") and (compBoard[0][4] != "X") and (compBoard[1][0] != "X") and (compBoard[1][1] != "X") and (compBoard[1][2] != "X") and (compBoard[1][3] != "X") and (compBoard[1][4] != "X") and (compBoard[2][0] != "X") and (compBoard[2][1] != "X") and (compBoard[2][2] != "X") and (compBoard[2][3] != "X") and (compBoard[2][4] != "X") and (compBoard[3][0] != "X") and (compBoard[3][1] != "X")and (compBoard[3][2] != "X") and (compBoard[3][3] != "X") and (compBoard[3][4] != "X") and (compBoard[4][0] != "X") and (compBoard[4][1] != "X") and (compBoard[4][2] != "X")and (compBoard[4][3] != "X") and (compBoard[4][4] != "X"):
         print("You win!")                #if all the X's are hit on the computer's board
         return 1
 
@@ -174,36 +180,46 @@ def main():
     """
 
     game = 0
+    playerScore = 0                 #player's initial score
+    compScore = 0                  #computer's initial score
 
     print("Let's play Battleship!")
 
-    board1 = [["[]","[]","[]","[]","[]"],
+    playerBoard = [["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"]]
 
-    board2 = [["[]","[]","[]","[]","[]"],
+    compBoard = [["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"],
              ["[]","[]","[]","[]","[]"]]
 
 
-    board1 = startBoard(board1)
-    board2 = startBoard(board2)
-    printBoard(board1)
+    playerBoard = startBoard(playerBoard)
+    compBoard = startBoard(compBoard)
+    printBoard(playerBoard)
     #print("")
-    #printBoard(board2)
+    #printBoard(compBoard)
 
-    while game == 0:
-        board2 = playerChoice(board2)
-        game = winGame(board1, board2)
-        #changeBoard(board2, game)
+    turns = 10
 
-        board1 = computerChoice(board1)
-        game = winGame(board1, board2)
-        changeBoard(board1, game)
+    while turns > 0 :
+        while game == 0:
+            compBoard = playerChoice(compBoard)
+            game = winGame(playerBoard, compBoard)
+            #changeBoard(compBoard, game)
+
+            playerBoard = computerChoice(playerBoard)
+            game = winGame(playerBoard, compBoard)
+            changeBoard(playerBoard, game)
+            compScore
+
+            turns -= 1
+
+            print(turns,"turns left")
 
 
 if __name__ == '__main__':
